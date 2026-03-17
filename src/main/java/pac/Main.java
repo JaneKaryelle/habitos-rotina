5b3482eed39902be05f597bc77144ae1ddc246ce
Janne <jannekaryelle0@gmail.com>
2026-03-17 20:49:03 -0300

    Crud

--- src/main/java/pac/Main.java
+++ src/main/java/pac/Main.java
@@ -3,16 +3,50 @@ package pac;
 import pac.dao.TarefaDAO;
 import pac.modelo.Tarefa;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Tarefa tarefa1 = new Tarefa();
-        tarefa1.setDescricao("Arrumar casa");
-        tarefa1.setFinalizada(Boolean.FALSE);
+        Scanner teclado = new Scanner(System.in);
 
-        TarefaDAO tarefaDAO = new TarefaDAO();
-        tarefaDAO.salvar(tarefa1);
+        // Pergunta ao usuario a tarefa
+        System.out.println("--- Sistema de Tarefas ---");
+        System.out.println("Qual tarefa você deseja salvar no banco de dados?");
+        System.out.print("Digite aqui: ");
 
 
+        String descricaoDigitada = teclado.nextLine();
+
+        Tarefa novaTarefa = new Tarefa();
+
+
+        novaTarefa.setDescricao(descricaoDigitada);
+        novaTarefa.setFinalizada(false); // Como é uma tarefa nova, ela fica como "não finalizada"
+
+
+        TarefaDAO operario = new TarefaDAO();
+        operario.salvar(novaTarefa);
+
+        System.out.println("✅ Tarefa '" + descricaoDigitada + "' salva com sucesso!");
+
+        //lista todas as tarefas do banco
+        TarefaDAO operacao = new TarefaDAO();
+        operacao.listarTodas();
+
+        // UPDATE no banco de dados
+        TarefaDAO operario2 = new TarefaDAO();
+
+            Tarefa tarefaModificada = new Tarefa();
+            tarefaModificada.setId(3L);
+            tarefaModificada.setDescricao("Trocar a torneira");
+            tarefaModificada.setFinalizada(true);
+            operario2.atualizar(tarefaModificada);
+
+        //Excluir tarefa
+        TarefaDAO operacao3 = new TarefaDAO();
+        operacao3.excluir(3L);
+        operacao3.listarTodas();
     }
+
 }
--- src/main/java/pac/dao/TarefaDAO.java
+++ src/main/java/pac/dao/TarefaDAO.java
@@ -4,6 +4,7 @@ import pac.infra.ConnectionFactory;
 import pac.modelo.Tarefa;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TarefaDAO {
@@ -13,8 +14,9 @@ public class TarefaDAO {
     public TarefaDAO() {
         this.fabrica = new ConnectionFactory();
     }
+
     public void salvar(Tarefa tarefa) {
-        try(Connection conexao = fabrica.recuperarConexao()) {
+        try (Connection conexao = fabrica.recuperarConexao()) {
             String sql = "INSERT INTO tarefa (descricao, finalizada) VALUES (?, ?)";
             PreparedStatement stmt = conexao.prepareStatement(sql);
             stmt.setString(1, tarefa.getDescricao());
@@ -25,7 +27,50 @@ public class TarefaDAO {
             System.out.println("Salvando uma tarefa... " + tarefa.getDescricao());
 
         } catch (SQLException e) {
-            throw new RuntimeException("Erro ao salvar tarefa no banco!",e);
+            throw new RuntimeException("Erro ao salvar tarefa no banco!", e);
+        }
+    }
+
+    public void listarTodas() {
+
+        String sql = "SELECT * FROM tarefa";
+        try (Connection conexao = fabrica.recuperarConexao()) {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            ResultSet relatorio = stmt.executeQuery();
+
+            while (relatorio.next()) {
+                Long id = relatorio.getLong("id");
+                String descricao = relatorio.getString("descricao");
+                boolean finalizada = relatorio.getBoolean("finalizada");
+                System.out.println(id + "- Tarefa: " + descricao + " - " + finalizada);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar tarefas", e);
+        }
+    }
+
+    public void atualizar(Tarefa tarefa) {
+        String sql = "UPDATE tarefa SET descricao = ?, finalizada = ? WHERE id = ?";
+        try (Connection conexao = fabrica.recuperarConexao()) {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, tarefa.getDescricao());
+            stmt.setBoolean(2, tarefa.isFinalizada());
+            stmt.setLong(3, tarefa.getId());
+            stmt.execute();
+            System.out.println("Atualizado com sucesso!");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar tarefas no banco!", e);
+        }
+    }
+
+    public void excluir(Long id) {
+        String sql = "DELETE FROM tarefa WHERE id = ?";
+        try (Connection conexao = fabrica.recuperarConexao()) {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setLong(1, id);
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao excluir tarefas no banco!", e);
         }
     }
 }
